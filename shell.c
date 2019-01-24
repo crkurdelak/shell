23cbec64ccb6f6e885d5a26279bcf2ec07b56677
Brian Snider <bsnider@georgefox.edu>
2019-01-23 23:32:11 -0800

    add shell starter code

---
+++ shell.c
@@ -0,0 +1,23 @@
+/**
+ * @file shell.c
+ * @brief A command-line shell program.
+ *
+ * A command-line shell program that uses system calls to directly invoke
+ * operating system services, rather than relying on C library calls.
+ *
+ * @author Brian R. Snider (bsnider@georgefox.edu)
+ */
+
+#include <sys/syscall.h>
+#include "shell.h"
+
+/**
+ * @brief Main entry point of the shell.
+ *
+ * @return The exit status of the shell.
+ */
+int main() {
+    // TODO implement shell
+
+    return 0;
+}
---
+++ shell.h
@@ -0,0 +1,13 @@
+/**
+ * @file shell.h
+ * @brief Function prototypes for the shell.
+ *
+ * @author Brian R. Snider (bsnider@georgefox.edu)
+ */
+
+#ifndef SHELL_H
+#define SHELL_H
+
+// TODO add function prototypes
+
+#endif /* SHELL_H */
