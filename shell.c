650d2d1251bf516e4aff34f51915d2237ea3abc6
crkur <ckurdelak20@georgefox.edu>
2023-01-17 15:47:52 -0800

    cloned starter repo

--- shell.c
+++ shell.c
@@ -5,7 +5,7 @@
  * A command-line shell program that uses system calls to directly invoke
  * operating system services, rather than relying on C library calls.
  *
- * @author Brian R. Snider (bsnider@georgefox.edu)
+ * @author Ceci Kurdelak (ckurdelak20@georgefox.edu)
  */
 
 #include <sys/syscall.h>
@@ -18,6 +18,13 @@
  */
 int main() {
     // TODO implement shell
+    /*
+    Your first (and intentionally very minimal) version of the shell should do the following:
+    Write a shell$ prompt to standard output;
+    Read a user command (max length 255 characters) from standard input;
+    "Execute" the command by simply writing the command back to standard output;
+    If the command is exit, then exit; else, repeat from step 1.
+     */
 
     return 0;
 }
