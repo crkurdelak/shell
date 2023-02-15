f78ff722fe76da244fc460d65cfa978600b4907b
crkur <ckurdelak20@georgefox.edu>
2023-02-15 12:18:15 -0800

    created new files

---
+++ shellcmd.cpp
@@ -0,0 +1,3 @@
+//
+// Created by crkur on 2/15/2023.
+//
---
+++ shellcmd.h
@@ -0,0 +1,10 @@
+//
+// Created by crkur on 2/15/2023.
+//
+
+#ifndef SHELL_SHELLCMD_H
+#define SHELL_SHELLCMD_H
+
+#endif //SHELL_SHELLCMD_H
+
+// TODO migrate commands to functions
